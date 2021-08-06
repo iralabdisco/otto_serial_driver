75dfe14db32c8dc188203336cbd724a814af3308
Federica Di Lauro <federicadilauro1998@gmail.com>
2021-08-06 14:52:27 +0200

    transmit parameters at startup

--- src/otto_serial_driver.py
+++ src/otto_serial_driver.py
@@ -45,6 +45,8 @@ def otto_serial_driver():
             time.sleep(2)
     rospy.loginfo(serial_port + ' opened')
 
+    reset_st()
+
     ser.reset_output_buffer()
     ser.reset_input_buffer()
     
@@ -60,6 +62,38 @@ def otto_serial_driver():
     crc_tx = 0
     dim_rx = struct.calcsize('=HHLLL')
 
+    ticks_per_revolution = rospy.get_param("ticks_per_revolution", 148000)
+    baseline = rospy.get_param("baseline", 0.435)
+
+    left_wheel_circ = rospy.get_param("left_wheel_circ", 0.789)
+    right_wheel_circ = rospy.get_param("right_wheel_circ", 0.783)
+
+    kp_left = rospy.get_param("kp_left", 180)
+    ki_left = rospy.get_param("ki_left", 200)
+    kd_left = rospy.get_param("kd_left", 0)
+
+    kp_right = rospy.get_param("kp_right", 185)
+    ki_right = rospy.get_param("ki_right", 195)
+    kd_right = rospy.get_param("kd_right", 0)
+
+    kp_cross = rospy.get_param("kp_cross", 0)
+    ki_cross = rospy.get_param("ki_cross", 0)
+    kd_cross = rospy.get_param("kd_cross", 0)
+
+    config_msg = struct.pack('<Lffffffffffff', ticks_per_revolution,
+                                baseline,
+                                left_wheel_circ, right_wheel_circ,
+                                kp_left, ki_left, kd_left, 
+                                kp_right, ki_right, kd_right,
+                                kp_cross, ki_cross, kd_cross)
+    ser.write(config_msg)
+    crc_tx = Crc32Mpeg2.calc(config_msg)
+    # convert crc to bytes and send
+    crc_tx_pack = struct.pack('<L', crc_tx)
+    ser.write(crc_tx_pack)
+
+    rate.sleep()
+    rate.sleep()
     while (not rospy.is_shutdown()):
 
         ## Transmit
@@ -68,11 +102,11 @@ def otto_serial_driver():
         ser.write(vel_msg)
         # calculate crc on byte buffer
         crc_tx = Crc32Mpeg2.calc(vel_msg)
-        # comvert crc to bytes and send
+        # convert crc to bytes and send
         crc_tx_pack = struct.pack('<L', crc_tx)
         ser.write(crc_tx_pack)
         
-        rospy.logdebug("Sent cmd_vel. Linear %f, Angular %f", linear_vel, angular_vel)
+        rospy.logdebug("Sent cmd_vel: Linear %f, Angular %f", linear_vel, angular_vel)
         ## Receive
         # read from serial
         buffer_rx = ser.read(12)
@@ -83,9 +117,24 @@ def otto_serial_driver():
         crc_rx_calc = Crc32Mpeg2.calc(buffer_rx)
         if (crc_rx_calc != crc_rx):
             rospy.logerr("Error on rx (incorrect CRC)")
-        
-        rospy.logdebug("Received otto status [status, delta_millis, left_ticks, right_ticks, crc[")
-        rospy.logdebug(status_msg)
+        if (status_msg[0] != 1):
+            rospy.logerr("Otto error!")
+            if (status_msg[0] == 2):
+                rospy.logerr("Error on receiving config! Shutting down")
+                rospy.signal_shutdown
+            elif (status_msg[0] == 3):
+                rospy.logerr("Error on receiving vel_msg!")
+            elif (status_msg[0] == 4):
+                rospy.logerr("H-Bridge fault!, Shutting down")
+                rospy.signal_shutdown
+            else:
+                rospy.logerr("Unknown otto status! Shutting down")
+                rospy.signal_shutdown
+            
+        rospy.logdebug("Received otto status:")
+        rospy.logdebug("Status %d, Delta_millis %d, Left_ticks %d, Right_ticks %d", 
+                        status_msg[0], status_msg[1], status_msg[2], status_msg[3])
+                        
         rate.sleep()
 
 def reset_st():

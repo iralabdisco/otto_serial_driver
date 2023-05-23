fix covariance

src/otto_serial_driver_node.py
@@ -189,8 +189,8 @@ def otto_serial_driver():
             odom.pose.pose = Pose(Point(global_x, global_y, 0.), Quaternion(*odom_quat))
             odom.child_frame_id = "base_link"
             odom.twist.twist = Twist(Vector3(lin_vel, 0, 0), Vector3(0, 0, ang_vel))
-            odom.pose.covariance = np.eye(6)*0.1
-            odom.twist.covariance = np.eye(6)*0.05
+            odom_to_publish.pose.covariance = (np.eye(6)*0.02).flatten()
+            odom_to_publish.twist.covariance = (np.eye(6)*0.01).flatten()
             odom_pub.publish(odom)
 
         rospy.logdebug("Received otto status:")